f01f4774ad7e23a8dc54493213fea8190d0e74e8
dwl21 <nggus5@gmail.com>
2026-04-08 13:17:54 +0900

    chore: Set up project structure with Gradle, JDK 21, and initial boilerplate

---
+++ src/test/java/racingcar/ApplicationTest.java
@@ -0,0 +1,13 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class ApplicationTest {
+
+    @Test
+    void 기능_테스트() {
+        // TODO: 기능 테스트 작성
+    }
+}
